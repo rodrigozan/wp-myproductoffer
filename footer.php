6e24e37cda8eff60b06b5d16f889001e73f29dbd
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2025-07-24 13:54:20 -0300

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="site-footer">
+  <div class="container">
+    <p>&copy; <?php echo date('Y'); ?> MyProductOffer.com - All rights reserved</p>
+  </div>
+  <?php wp_footer(); ?>
+</footer>
+</body>
+</html>
